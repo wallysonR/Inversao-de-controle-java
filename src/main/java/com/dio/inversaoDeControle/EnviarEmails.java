13341f7dfee12d24b9f13a5e9deda66c5efd474b
Wallyson Roberto Leite dos Santos <wallysons@ciandt.com>
2022-10-19 15:33:56 -0300

    criacao de codigo acomplado

---
+++ src/main/java/com/dio/inversaoDeControle/EnviarEmails.java
@@ -0,0 +1,11 @@
+package com.dio.inversaoDeControle;
+
+public class EnviarEmails {
+    public EnviarEmails(String tipo, String endereco, String senha) {
+
+    }
+
+    public void retornar(String mensagem) {
+        System.out.println("Email enviado !");
+    }
+}
